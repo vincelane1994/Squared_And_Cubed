86cdee5cd2afe09979d0fbc13db8921c4b7cf0fe
vincelane1994 <vincelane1994@gmail.com>
2019-06-25 16:23:10 -0400

    Added multiplication table and made everything accessable through the console

--- src/LabFour.java
+++ src/LabFour.java
@@ -6,18 +6,20 @@ public class LabFour {
 		Scanner scnr = new Scanner(System.in);
 		int userNum;
 		char cont = 'y';
-
+		char viewTable = 'y';
+		char codeBat = 'y';
+		
 		int i = 1;
-		while (cont == 'y') {//So long as the user chooses to run this again it will loop.
+		while (cont == 'y') {// So long as the user chooses to run this again it will loop.
 			System.out.println("Enter your Number: ");
 			userNum = scnr.nextInt();
 			System.out.printf("%-12s | %-10s | %-10s\n", "User Number", "Squared", "Cubed");// Top of the chart
 
 			while (i <= userNum) {
-				System.out.printf("%-12s | %-10s | %-10s\n", i, (i * i), (i * i * i));//Produces the lines of the chart
-				i++;//adds one to the variable to increase the base number each line.
+				System.out.printf("%-12s | %-10s | %-10s\n", i, (i * i), (i * i * i));// Produces the lines of the chart
+				i++;// adds one to the variable to increase the base number each line.
 			}
-			i = 1;//Resets i so that the user can enter a new number
+			i = 1;// Resets i so that the user can enter a new number
 			System.out.println(" ");// Space for Visual esthetics
 			System.out.println("Would you like to try another number? (Y/N)");// Option to do another
 
@@ -25,9 +27,40 @@ public class LabFour {
 			cont = Character.toLowerCase(cont); // Changes character to lower case so capitalization doesn't matter
 
 		}
-		System.out.println("Goodbye!");
-		scnr.close();
+		System.out.println("Would you like to see a multiplication table? (y/n)");
 
+		viewTable = scnr.next().charAt(0); // Gets first character so that the user can type in yes or no
+		viewTable = Character.toLowerCase(viewTable); // Changes character to lower case so capitalization doesn't matter
+
+		if (viewTable == 'y') {
+
+			System.out.printf("%-5s | %-5s | %-5s | %-5s | %-5s | %-5s | %-5s | %-5s | %-5s | %-5s | %-5s\n", " ", "1",
+					"2", "3", "4", "5", "6", "7", "8", "9", "10");
+			System.out.printf("%-5s | %-5s | %-5s | %-5s | %-5s | %-5s | %-5s | %-5s | %-5s | %-5s | %-5s\n", "-----",
+					"-----", "-----", "-----", "-----", "-----", "-----", "-----", "-----", "-----", "-----");
+
+			int sideNum = 1;
+			while (sideNum <= 10) {
+				System.out.printf("%-5s | %-5s | %-5s | %-5s | %-5s | %-5s | %-5s | %-5s | %-5s | %-5s | %-5s\n",
+						(sideNum), (sideNum * 1), (sideNum * 2), (sideNum * 3), (sideNum * 4), (sideNum * 5),
+						(sideNum * 6), (sideNum * 7), (sideNum * 8), (sideNum * 9), (sideNum * 10));
+				sideNum = sideNum + 1;
+			}
+			System.out.println("Would you like to see my answer to the CodingBat? (y/n)");
+
+			codeBat = scnr.next().charAt(0); // Gets first character so that the user can type in yes or no
+			codeBat = Character.toLowerCase(codeBat); // Changes character to lower case so capitalization doesn't matter
+
+			if (codeBat == 'y') {
+				System.out.println("public boolean nearHundred(int n) {");
+				System.out.println("	 int value1 = 100-n;");
+				System.out.println("	 int value2 = 200-n;");
+				System.out.println("	 return (Math.abs(value1) <= 10 || Math.abs(value2) <= 10);");
+				System.out.println("}");
+
+				}
+			scnr.close();
+		}
 	}
 }
 
